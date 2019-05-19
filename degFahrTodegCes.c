ff863e7ef5c1adaf54fe154f4b6c5b791af1b182
akshayarise <akshayawakearise@gmail.com>
2019-05-19 20:31:00 +0530

    one file is added

---
+++ degFahrTodegCes.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+
+int main(){
+ int degFahr, degCel, step, limit;
+
+printf("Enter the degFahr, step, limit");
+scanf("%d%d%d",&degFahr,&step,&limit); 
+ 
+printf("degFahr = %d\n",degFahr);
+printf("step = %d\n", step);
+printf("limit = %d\n", limit);
+
+ return 0;
+}
